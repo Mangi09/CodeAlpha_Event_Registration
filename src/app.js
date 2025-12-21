f3cd0d73ee36ccf464fd1b60ff48c289e1d83e57
Mangi09 <poojarydwithi9@gmail.com>
2025-12-21 19:52:57 +0530

    Serve frontend using Express static middleware

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const eventRoutes = require("./routes/eventRoutes");
 const registrationRoutes = require("./routes/registrationRoutes");
-
+const path = require("path");
 const app = express();
 app.use(express.json());
 
+app.use(express.static(path.join(__dirname, "../public")));
 app.use("/api/events", eventRoutes);
 app.use("/api/registrations", registrationRoutes);
 
